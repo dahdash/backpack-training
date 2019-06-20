7a175631f3d75ff72a89ce437757ba0eaea03887
mmd <m.golmoradi@gmail.com>
2019-06-21 00:49:47 +0430

    add custom view and supplier button

---
+++ resources/views/vendor/backpack/crud/officeSuppliers.blade.php
@@ -0,0 +1,55 @@
+@extends('backpack::layout')
+
+@section('header')
+	<section class="content-header">
+	  <h1>{{$entry->name}}
+        <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+        <small>{!! $crud->getSubheading() ?? trans('backpack::crud.edit').' '.$crud->entity_name !!}.</small>
+	  </h1>
+	  <ol class="breadcrumb">
+	    <li><a href="{{ url(config('backpack.base.route_prefix'),'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
+	    <li><a href="{{ url($crud->route) }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
+	    <li class="active">{{ trans('backpack::crud.edit') }}</li>
+	  </ol>
+	</section>
+@endsection
+
+@section('content')
+@if ($crud->hasAccess('list'))
+	<a href="{{ url($crud->route) }}" class="hidden-print"><i class="fa fa-angle-double-left"></i> {{ trans('backpack::crud.back_to_all') }} <span>{{ $crud->entity_name_plural }}</span></a>
+@endif
+
+<div class="row m-t-20">
+	<div class="{{ $crud->getEditContentClass() }}">
+		<!-- Default box -->
+
+		@include('crud::inc.grouped_errors')
+
+		  <form method="post"
+		  		action="{{ url($crud->route.'/'.$entry->getKey().'/suppliers') }}"
+				@if ($crud->hasUploadFields('OfficeSuppliers', $entry->getKey()))
+				enctype="multipart/form-data"
+				@endif
+		  		>
+		  {!! csrf_field() !!}
+		  {!! method_field('POST') !!}
+		  <div class="col-md-12">
+		    <div class="row display-flex-wrap">
+		      <!-- load the view from the application if it exists, otherwise load the one in the package -->
+		      @if(view()->exists('vendor.backpack.crud.form_content'))
+		      	@include('vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'postOfficeSuppliers'])
+		      @else
+		      	@include('crud::form_content', ['fields' => $fields, 'action' => 'postOfficeSuppliers'])
+		      @endif
+		    </div><!-- /.box-body -->
+
+            <div class="">
+
+                @include('crud::inc.form_save_buttons')
+
+		    </div><!-- /.box-footer-->
+		  </div><!-- /.box -->
+		  </form>
+	</div>
+</div>
+@endsection
