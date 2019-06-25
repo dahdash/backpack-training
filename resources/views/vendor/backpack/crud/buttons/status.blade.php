a99e7a3f03761479bffa457ee0844eddecae0f32
mmd <m.golmoradi@gmail.com>
2019-06-26 03:00:49 +0430

    Customize Enable/Disable Icon

--- resources/views/vendor/backpack/crud/buttons/status.blade.php
+++ resources/views/vendor/backpack/crud/buttons/status.blade.php
@@ -1,7 +1,13 @@
+
 @if ($crud->hasAccess('update'))
 <a href="{{ url($crud->route.'/'.$entry->getKey().'/status') }} " class="btn btn-xs btn-default"
-  onclick="return confirm('Are you sure?')">
-  <i class="fa fa-check"></i> {{ $entry->enabled == 0 ? 'Enable' : 'Disable' }}
+  onclick="return confirm('Are you sure?')" >
+  @if($entry->enabled == 0)
+    <i class="fa fa-check"></i>
+  @endif
+  @if($entry->enabled == 1)
+    <i class="fa fa-ban"></i>
+  @endif
+  {{ $entry->enabled == 0 ? 'Enable' : 'Disable' }}
 </a>
-
 @endif
